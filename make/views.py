45199718337f4632584ca2d2704ed7d5ee915495
ryan <ryan@ryan-VirtualBox.(none)>
2014-09-15 19:34:21 +0800

    content

--- make/views.py
+++ make/views.py
@@ -22,26 +22,3 @@ class IndexView(SessionWizardView, SuccessMessageMixin):
                 setattr(new, k, v)
         new.save()
         return HttpResponseRedirect('/#form')
-
-
-'''
-class SignupPage(SuccessMessageMixin, CreateView):
-    model = Contact
-    template_name = 'signup.html'
-    success_message = "You have successfuly registered for Bloc Shot 2014 :)"
-
-    def get_success_url(self):
-        return reverse_lazy('signup_page')
-
-    def get_context_data(self, **kwargs):
-        context = super(SignupPage, self).get_context_data(**kwargs)
-        return context
-
-    def form_valid(self, form):
-        form.instance.designer = self.request.user
-        return super(SignupPage, self).form_valid(form)
-
-    def form_invalid(self, form):
-        print form.errors
-        return self.render_to_response(self.get_context_data(form=form))
-'''
